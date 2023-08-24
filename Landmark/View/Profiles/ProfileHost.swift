23435600bf4e81c1aad44dcf21fad1885e3ff161
rizkisiraj <jomirwa@gmail.com>
2023-08-24 20:55:09 +0700

    initial commit

--- Landmark/View/Profiles/ProfileHost.swift
+++ Landmark/View/Profiles/ProfileHost.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
     @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
 
+
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
-                .environmentObject(modelData)
+            HStack {
+                
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
+                Spacer()
+                EditButton()
+            }
+
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+                
+            }
         }
         .padding()
     }
@@ -22,7 +47,9 @@ struct ProfileHost: View {
 
 
 struct ProfileHost_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         ProfileHost()
             .environmentObject(ModelData())
     }
